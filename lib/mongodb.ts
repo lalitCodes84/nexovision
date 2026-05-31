578307fa075c9ec4335c50e98393e8c2185ff4e2
Lalit Sharma <vickysharma71za@gmail.com>
2026-05-31 21:24:00 +0530

    Email functionality added

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,7 +1,6 @@
 import { MongoClient, type Db } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
-console.log("Error",uri)
 const dbName = process.env.MONGODB_DB ?? "nexovision";
 
 if (!uri) {
